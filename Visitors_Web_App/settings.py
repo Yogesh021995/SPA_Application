d90927ca6616c87e49b2986bc5a49d1216b95a75
Yogesh Gadekar <gadekaryogesh2@gmail.com>
2023-09-04 18:14:41 +0530

    update changes

--- Visitors_Web_App/settings.py
+++ Visitors_Web_App/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'spa_app',
+    'rest_framework'
 ]
 
 MIDDLEWARE = [
--- spa_app/admin.py
+++ spa_app/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
-
+from .models import Visitor,StaffMember,VisitorDetails,Drink,VisitorDrink
 # Register your models here.
+
+class VisitorAdmin(admin.ModelAdmin):
+    list_display = ['name','mobile','address']
+
+admin.site.register(Visitor,VisitorAdmin)
+
+class StaffMemberAdmin(admin.ModelAdmin):
+    list_display = ['name','image','email','mobile']
+
+admin.site.register(StaffMember,StaffMemberAdmin)
